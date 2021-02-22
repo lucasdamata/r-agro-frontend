32e27a3b918cc06eedfef49ca495c83c45e15b5e
Lucas da Mata <lucas.mata@gruposetta.com.br>
2021-02-22 17:23:10 -0300

    Finalizando layout Cadastro

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -5,6 +5,7 @@ import Sidebar from '../components/Sidebar';
 import Input from '../components/Form'
 
 import '../styles/pages/cadastro.css'
+import {FiPlusSquare} from 'react-icons/fi'
 
 
 interface FormData {
@@ -61,31 +62,43 @@ function Cadastro(){
             <fieldset>  
              
               <div className='colum01'>
-                <Input className="os"  name="os" /> 
-                <Input className="location"  name="location" />
-              </div>
-              
-              <Scope path='time'>
-                <div className="colum02">
+                <label htmlFor="labelOS">OS</label>
+                <Input  className="os"  name="os" /> 
+                <Scope path='time'>
+                <label htmlFor="labelInitial">Inicio</label>
                 <Input type='time' className="initial"  name="initial" />
+                <label htmlFor="labelFinal">Final</label>
                 <Input type='time' className="final"  name="final" />
+                <label htmlFor="labelDate">Data</label>
                 <Input type='date' className="date"  name="date" />
-                </div>
               </Scope>
+              </div>
+        
+              <div className="colum02">
+              <label htmlFor="labelLocation">Lugar</label>
+              <Input className="location"  name="location" />
+              </div>
               
               <Scope path='machine'>
                 <div className="colum03">
+                <label htmlFor="labelNameMachine">Máquina</label>
                   <Input className="nameMachine"  name="nameMachine" />
+                <label htmlFor="labelImplementMachine">Complemento</label>
                   <Input className="inplementMachine"  name="inplementMachine" />
+                  <button type='button'   className='buttonMachine'  onClick={()=>{}}><FiPlusSquare size={30}  /></button>
                 </div>
               </Scope>
-
-              <Input className="operator"  name="operator" />
-
+              <div className="colum05">
+              <label htmlFor="labelOperator">Operador</label>
+                  <Input className="operator"  name="operator" />
+              </div>
               <Scope path='inputs'>
                 <div className="colum04">
+                <label htmlFor="labelFinal">Insumo</label>
                   <Input className="input"  name="input" />
+                <label htmlFor="labelFinal">Quantidade</label>
                   <Input  type='number' className="amount"  name="amount" />
+                  <button type='button'   className='buttonInput'  onClick={()=>{}}><FiPlusSquare size={30}  /></button>
                 </div>
               </Scope>
               
